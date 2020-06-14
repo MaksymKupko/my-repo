c4d2eac79accc2e9bcea96d809e52e63aedc13b4
Maksym Kupko <maksym.kupko11@gmail.com>
2020-06-14 17:56:38 +0300

    Added fetching films via search input. Reacts with delay after input

--- movieFight/index.js
+++ movieFight/index.js
@@ -1,11 +1,18 @@
-const fetchData = async () => {
+const fetchData = async (searchTerm) => {
     const response = await axios.get("http://www.omdbapi.com", {
         params: {
             apikey: "a19cdc15",
-            s: "avengers"
+            s: searchTerm,
         },
     });
     console.log(response);
-    
 };
-fetchData()
+const input = document.querySelector("input");
+
+let timeoutID = null;
+input.addEventListener("input", ({ target }) => {
+    if (timeoutID) {
+        clearTimeout(timeoutID);
+    }
+    timeoutID = setTimeout(() => fetchData(target.value), 500);
+});
