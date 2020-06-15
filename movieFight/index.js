eb0bf5a034b205f107e426eb47c0753a3de31baf
Maksym Kupko <maksym.kupko11@gmail.com>
2020-06-15 17:51:24 +0300

    Added fetching data about selected movie and showind it's details on the page.

--- movieFight/index.js
+++ movieFight/index.js
@@ -18,9 +18,9 @@ root.innerHTML = `
     <label><b>Search For A Movie</b></label>
     <input class="input">
     <div class="dropdown">
-    <div class="dropdown-menu">
-    <div class="dropdown-content results"></div>
-    </div>
+        <div class="dropdown-menu">
+            <div class="dropdown-content results"></div>
+        </div>
     </div>
 `;
 
@@ -40,18 +40,19 @@ const onInput = async ({ target }) => {
     dropdown.classList.add("is-active");
 
     const moviesArr = movies.map((movie) => {
-        const { Title, Poster } = movie;
+        const { Title, Poster, imdbID } = movie;
         const option = document.createElement("a");
         option.classList.add("dropdown-item");
         option.addEventListener("click", () => {
             dropdown.classList.remove("is-active");
             input.value = Title;
+            onMovieSelect(imdbID);
         });
 
         const imgSRC = Poster === "N/A" ? "" : Poster;
         option.innerHTML = `
-                    <img src="${imgSRC}">
-                    ${Title}
+            <img src="${imgSRC}">
+            ${Title}
         `;
 
         return option;
@@ -67,3 +68,57 @@ document.addEventListener("click", ({ target }) => {
         dropdown.classList.remove("is-active");
     }
 });
+
+const onMovieSelect = async (movieID) => {
+    const response = await axios.get("http://www.omdbapi.com", {
+        params: {
+            apikey: "a19cdc15",
+            i: movieID,
+        },
+    });
+    document.querySelector("#summary").innerHTML = movieTemplate(response.data);
+    console.log(response.data);
+    
+};
+
+const movieTemplate = (movieDetails) => {
+    const { Poster, Title, Genre, Plot, Awards, BoxOffice,Metascore, imdbRating, imdbVotes } = movieDetails;
+
+    return `
+        <article class="media">
+            <figure class="media-left">
+                <p class="image">
+                    <img src="${Poster}" />
+                </p>
+            </figure>
+            <div class="media-content">
+                <div class="content">
+                    <h1>${Title}</h1>
+                    <h4>${Genre}</h4>
+                    <p>${Plot}</p>
+                </div>
+            </div>
+        </article>
+        <article class="notification is-primary">
+            <p class="title">${Awards}</p>
+            <p class="subtitle">Awards</p>
+        </article>
+        <article class="notification is-primary">
+            <p class="title">${BoxOffice}</p>
+            <p class="subtitle">Box Office</p>
+        </article>
+        <article class="notification is-primary">
+            <p class="title">${Metascore}</p>
+            <p class="subtitle">Metascore</p>
+        </article>
+        <article class="notification is-primary">
+            <p class="title">${imdbRating}</p>
+            <p class="subtitle">IMDB Rating</p>
+        </article>
+        <article class="notification is-primary">
+            <p class="title">${imdbVotes}</p>
+            <p class="subtitle">IMDB Votes</p>
+        </article>
+        
+    `;
+};
