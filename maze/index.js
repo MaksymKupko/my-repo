0f5fffcd3710718dea736e38a54cb2fb5ea52204
Maksym Kupko <maksym.kupko11@gmail.com>
2020-06-18 19:31:05 +0300

    User wins when touching the goal

--- maze/index.js
+++ maze/index.js
@@ -1,4 +1,4 @@
-const { Engine, Render, Runner, World, Bodies, Body } = Matter;
+const { Engine, Render, Runner, World, Bodies, Body, Events } = Matter;
 
 const cells = 3;
 const width = 600;
@@ -150,12 +150,13 @@ verticals.forEach((row, rowIndex) => {
 
 const goal = Bodies.rectangle(width - unitLength / 2, height - unitLength / 2, unitLength * 0.7, unitLength * 0.7, {
     isStatic: true,
+    label: "goal",
 });
 World.add(world, goal);
 
 // Ball
 
-const ball = Bodies.circle(unitLength / 2, unitLength / 2, unitLength / 4);
+const ball = Bodies.circle(unitLength / 2, unitLength / 2, unitLength / 4, { label: "ball" });
 World.add(world, ball);
 
 document.addEventListener("keydown", ({ keyCode }) => {
@@ -173,4 +174,16 @@ document.addEventListener("keydown", ({ keyCode }) => {
     if (keyCode === 65) {
         Body.setVelocity(ball, { x: x - 5, y });
     }
+});
+
+// Win condition
+
+Events.on(engine, "collisionStart", (event) => {
+    event.pairs.forEach((collision) => {
+        const labels = ["ball", "goal"];
+
+        if (labels.includes(collision.bodyA.label) && labels.includes(collision.bodyB.label)) {
+            
+        }
+    });
 });
