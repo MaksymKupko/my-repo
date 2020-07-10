a12783c115d21c6cbd0e9b12ae0bf7b7cd4e61bd
Maksym Kupko <maksym.kupko11@gmail.com>
2020-07-11 01:13:44 +0300

    Changed api key and made it hidden

--- reactPractice/videos/src/apis/youtube.js
+++ reactPractice/videos/src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const KEY = "AIzaSyCJBwO3gwtnkbTAIS1FDg6UNak7iF0XxmI";
+const KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
